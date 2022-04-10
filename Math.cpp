6c2118a128c77a6d467ab86a578e4ffa0f000df1
Omar <88511754+Jamlee977@users.noreply.github.com>
2022-04-10 14:08:15 +0300

    Separated the Str class

--- Math.cpp
+++ Math.cpp
@@ -1,32 +1,5 @@
 // Omar Estietie
-
-#include <iostream>
-#include <sstream>
-using std::string, std::stringstream;
-
-
-class Str {
-private: 
-    const double __HUGE_NUMBER  =      1e+308;
-    const float Infinity        =      ((float)(__HUGE_NUMBER * __HUGE_NUMBER));
-    const float NaN             =      ((float)Infinity * 0.0F);
-    const double minInfinity    =      Infinity * -1;
-public:
-    /**
-    * @brief Convert a number to a string.
-    * 
-    * @param numberToString The number that the user needs to turn into a string. 
-    * @param convertedToString The name of a variable that will contain the new string value.
-    * @return string
-    */
-    string toString(long double numberToString, string &convertedToString) {
-    if (numberToString == NaN || numberToString == Infinity || numberToString == minInfinity) return "Invalid";
-        stringstream convertToString;
-        convertToString << numberToString;
-        convertToString >> convertedToString;
-        return convertedToString;
-    }
-};
+#include "src/str.h"
 
 /**
  * @brief A math-class that can be used to do math operation.
