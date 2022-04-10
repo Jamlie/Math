20aa974eef3386e81576faef3ff9668e6ecd5b81
Omar <88511754+Jamlee977@users.noreply.github.com>
2022-04-10 14:09:11 +0300

    Str header to include the Str class

---
+++ src/str.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <sstream>
+
+#ifndef STR_H
+#define STR_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "str.cpp"
+
+class math;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // STR_H
